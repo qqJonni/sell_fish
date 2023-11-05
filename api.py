8504f0833351f5ebb51516dcd4937cdf1fb2a9f8
Valeriy Lvov <tepliy_shov@mail.ru>
2023-11-05 17:32:55 +0400

    add pictures

--- api.py
+++ api.py
@@ -5,12 +5,23 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 api = os.environ.get('API_TOKEN_SALT')
 
-response = requests.get('http://localhost:1337/api/products')
+response = requests.get('http://localhost:1337/api/products/2?populate=picture')
 response.raise_for_status()
 
 response_data = response.json()
 products = response_data['data']
 
-for product in products:
-    title = product['attributes']['title']
-    print(title)
+title = products['attributes']['title']
+picture = products['attributes']['picture']['data'][0]['attributes']['url']
+
+# for product in products:
+#     title = product['attributes']['title']
+#     # Проверьте, есть ли изображение в продукте
+#     if 'picture' in product['attributes']:
+#         picture_url = product['attributes']['picture'][0]['url']
+#         print(f"Заголовок: {title}, URL изображения: {picture_url}")
+#     else:
+#         print(f"Заголовок: {title}, Нет изображения")
+print(title)
+print(picture)
+
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,10 @@
 from aiogram import types, executor, Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputFile
 import os
 from dotenv import load_dotenv, find_dotenv
 import requests
+import io
 
 load_dotenv(find_dotenv())
 storage = MemoryStorage()
@@ -11,32 +12,66 @@ bot = Bot(os.environ.get('TELEGRAM_TOKEN'))
 dp = Dispatcher(bot, storage)
 
 api = os.environ.get('API_TOKEN_SALT')
-response = requests.get('http://localhost:1337/api/products')
-response.raise_for_status()
-response_data = response.json()
-products = response_data['data']
-
-product_descriptions = {product['attributes']['title']: product['attributes']['description'] for product in products}
+base_url = 'http://localhost:1337/api/products/'
 
 
-def get_kb():
+def fetch_product_data(product_id):
+    product_url = f'{base_url}{product_id}?populate=picture'
+    response = requests.get(product_url)
+    response.raise_for_status()
+    response_data = response.json()
+
+    product_data = response_data['data']['attributes']
+    title = product_data['title']
+    description = product_data['description']
+    price = product_data['price']
+
+    picture_data = product_data['picture']['data'][0]['attributes']
+    image_relative_url = picture_data['url']
+    image_url = f'http://localhost:1337{image_relative_url}'
+
+    return {
+      'title': title,
+      'description': description,
+      'price': price,
+      'image_url': image_url
+    }
+
+
+def get_kb(products):
     kb = InlineKeyboardMarkup(row_width=1)
     for product in products:
         title = product['attributes']['title']
-        kb.add(InlineKeyboardButton(title, callback_data=f'product_{title}'))
+        kb.add(InlineKeyboardButton(title, callback_data=f'product_{product["id"]}'))
     return kb
 
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer('Добро пожаловать! Выберите товар:', reply_markup=get_kb())
+    response = requests.get(base_url)
+    response.raise_for_status()
+    products_data = response.json()['data']
+    await message.answer('Добро пожаловать! Выберите товар:', reply_markup=get_kb(products_data))
 
 
 @dp.callback_query_handler(lambda c: c.data and c.data.startswith('product_'))
 async def process_product_callback(callback_query: types.CallbackQuery):
-    product_title = callback_query.data.split('_')[1]
-    description = product_descriptions.get(product_title, "Description not found")
-    await bot.send_message(callback_query.from_user.id, description)
+    product_id = int(callback_query.data.split('_')[1])
+    product_data = fetch_product_data(product_id)
+
+    description = product_data['description']
+    price = product_data['price']
+    image_url = product_data['image_url']
+    title = product_data['title']
+
+    response = requests.get(image_url)
+    if response.ok:
+        photo_binary = response.content
+        with io.BytesIO(photo_binary) as photo_stream:
+            photo = InputFile(photo_stream, filename=f"{product_data['title']}.jpg")
+            response_message = f"{title}\nЦена: {price}\n\n{description}"
+            await bot.send_photo(callback_query.from_user.id, photo, caption=response_message)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
