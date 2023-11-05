575946962be313d6d8863c1d4bfaa8d60cb8aa45
Valeriy Lvov <tepliy_shov@mail.ru>
2023-11-05 11:24:11 +0400

    add description in to telegram bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,21 +10,20 @@ storage = MemoryStorage()
 bot = Bot(os.environ.get('TELEGRAM_TOKEN'))
 dp = Dispatcher(bot, storage)
 
-
 api = os.environ.get('API_TOKEN_SALT')
 response = requests.get('http://localhost:1337/api/products')
 response.raise_for_status()
 response_data = response.json()
 products = response_data['data']
 
+product_descriptions = {product['attributes']['title']: product['attributes']['description'] for product in products}
+
 
 def get_kb():
     kb = InlineKeyboardMarkup(row_width=1)
-
     for product in products:
         title = product['attributes']['title']
-        kb.add(InlineKeyboardButton(title, callback_data='button'))
-
+        kb.add(InlineKeyboardButton(title, callback_data=f'product_{title}'))
     return kb
 
 
@@ -32,6 +31,12 @@ def get_kb():
 async def start(message: types.Message):
     await message.answer('Добро пожаловать! Выберите товар:', reply_markup=get_kb())
 
+
+@dp.callback_query_handler(lambda c: c.data and c.data.startswith('product_'))
+async def process_product_callback(callback_query: types.CallbackQuery):
+    product_title = callback_query.data.split('_')[1]
+    description = product_descriptions.get(product_title, "Description not found")
+    await bot.send_message(callback_query.from_user.id, description)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
